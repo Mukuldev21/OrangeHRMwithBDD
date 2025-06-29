7a93d228d13c151e83642226d19995832fc3bc7a
Mukuldev21 <mukul.com12@gmail.com>
2025-06-29 17:11:59 +0530

    Updated DriverManager

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -2,7 +2,11 @@ package utils;
 
     import org.openqa.selenium.WebDriver;
     import org.openqa.selenium.chrome.ChromeDriver;
+    import org.openqa.selenium.chrome.ChromeOptions;
     import org.openqa.selenium.firefox.FirefoxDriver;
+
+    import java.nio.file.Files;
+    import java.nio.file.Path;
     import java.util.Properties;
 
     public class DriverManager {
@@ -13,7 +17,16 @@ package utils;
                 case "firefox":
                     return new FirefoxDriver();
                 case "chrome":
-                    return new ChromeDriver();
+                    ChromeOptions options = new ChromeOptions();
+                    // Create a unique temp directory for Chrome's user data
+                    try {
+                        Path tempProfile = Files.createTempDirectory("chrome-profile-");
+                        options.addArguments("user-data-dir=" + tempProfile.toAbsolutePath().toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        // Optionally, you can continue with default options if temp dir fails
+                    }
+                    return new ChromeDriver(options);
                 default:
                     throw new IllegalArgumentException("Unsupported browser: " + browser);
             }
