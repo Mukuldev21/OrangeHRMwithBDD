77caeba15233beca71de66ce056d0b8c5f36bfa2
Mukuldev21 <mukul.com12@gmail.com>
2025-06-29 19:53:44 +0530

    Updated DriverManager

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -4,6 +4,9 @@ package utils;
         import org.openqa.selenium.chrome.ChromeDriver;
         import org.openqa.selenium.chrome.ChromeOptions;
         import org.openqa.selenium.firefox.FirefoxDriver;
+
+        import java.nio.file.Files;
+        import java.nio.file.Path;
         import java.util.Properties;
 
         public class DriverManager {
@@ -24,12 +27,24 @@ package utils;
                         ChromeOptions chromeOptions = new ChromeOptions();
                         chromeOptions.addArguments("--no-sandbox");
                         chromeOptions.addArguments("--disable-dev-shm-usage");
+                        try {
+                            Path tempProfile = Files.createTempDirectory("chrome-user-data");
+                            chromeOptions.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+                        } catch (Exception e) {
+                            throw new RuntimeException("Failed to create temp user data dir for Chrome", e);
+                        }
                         return new ChromeDriver(chromeOptions);
                     case "headless":
                         ChromeOptions options = new ChromeOptions();
                         options.addArguments("--headless=new");
                         options.addArguments("--no-sandbox");
                         options.addArguments("--disable-dev-shm-usage");
+                        try {
+                            Path tempProfile = Files.createTempDirectory("chrome-user-data");
+                            options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+                        } catch (Exception e) {
+                            throw new RuntimeException("Failed to create temp user data dir for Chrome", e);
+                        }
                         return new ChromeDriver(options);
                     default:
                         throw new IllegalArgumentException("Unsupported browser: " + browser);
